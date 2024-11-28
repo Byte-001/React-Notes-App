954fdf32586d0c529fa9088d6f689019c72a8ca8
ardakoca <ardakoca957@gmail.com>
2024-11-28 17:07:31 +0300

    chore: Add basic NoteList component structure

---
+++ notes-app/src/components/NoteList.jsx
@@ -0,0 +1,11 @@
+import "../App.css";
+
+const NoteList = () => {
+    return (
+        <div className={"note-list"}>
+            <p>note</p>
+        </div>
+    );
+}
+
+export default NoteList;
